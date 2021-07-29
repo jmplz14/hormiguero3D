aa37fa2f0086ce09d755d7afb63dbba452e38dd0
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2021-07-29 16:02:15 +0200

    Metemos dobleclick para centrar camara en una pieza y ocultar las demas

--- vista3D/static/javascript/piezas.js
+++ vista3D/static/javascript/piezas.js
@@ -110,7 +110,9 @@ function getMousePosition(event) {
 }*/
 let scene;
 let modelos = new Array(39);
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 300);
+camera.position.z = 100;
+camera.position.y = 30
 
 
 main();
@@ -127,6 +129,8 @@ function cargarSTL(position, loader, material, coordenadas, file) {
 }
 
 
+
+
 function main() {
     const canvas = document.querySelector('canvas');
 
@@ -136,9 +140,7 @@ function main() {
     const aspect = 2;  // the canvas default
     const near = 0.1;
     const far = 200;
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-    camera.position.z = 100;
-    camera.position.y = 30
+
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color('white');
@@ -150,6 +152,7 @@ function main() {
     cameraPole.add(camera);
 
     const control = new OrbitControls(camera, renderer.domElement);
+
 
     const color = 0xFFFFFF;
     const intensity = 1;
@@ -178,19 +181,64 @@ function main() {
             const intersectedObjects = this.raycaster.intersectObjects(scene.children);
             if (intersectedObjects.length) {
                 this.pickedObject = intersectedObjects[0].object;
-                alert(this.pickedObject.id);
+
                 /*// pick the first object. It's the closest one
                 this.pickedObject = intersectedObjects[0].object;
                 // save its color
                 this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
                 // set its emissive color to flashing red/yellow
                 this.pickedObject.material.emissive.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);*/
-                alert("hola");
-            } else {
-                alert("Adios")
+                moveCamera(this.pickedObject, camera);
             }
         }
     }
+    function getCenterPoint(objeto) {
+        var middle = new THREE.Vector3();
+        var geometry = objeto.geometry;
+    
+        geometry.computeBoundingBox();
+    
+        middle.x = (geometry.boundingBox.max.x + geometry.boundingBox.min.x) / 2;
+        middle.y = (geometry.boundingBox.max.y + geometry.boundingBox.min.y) / 2;
+        middle.z = (geometry.boundingBox.max.z + geometry.boundingBox.min.z) / 2;
+    
+        objeto.localToWorld( middle );
+        return middle;
+    }
+
+    function hideObjects(id) {
+        var i = 0;
+        scene.traverse(function (node) {
+
+            if (node instanceof THREE.Mesh) {
+                if (node.id !== id) {
+                    node.visible = false;
+                }
+
+            }
+
+        });
+        alert(i);
+    }
+    function moveCamera(object, camera) {
+        var middle = getCenterPoint(object);
+        camera.position.x = object.position.x;
+        camera.position.y = object.position.y;
+        camera.position.z = 50;
+
+        control.target.set(middle.x,middle.y, middle.z);
+        control.update();
+        
+        hideObjects(object.id);
+        
+        /*const geometry = new THREE.SphereGeometry(5, 32, 32);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.position.set(middle.x,middle.y, middle.z);
+        scene.add(sphere);*/
+        render();
+    }
+
 
     const pickPosition = { x: 0, y: 0 };
     const pickHelper = new PickHelper();
@@ -200,62 +248,58 @@ function main() {
     clearPickPosition();
 
     //bases
-    cargarSTL(0, loader, materialSTL, new THREE.Vector3(40, 0, 0), "/static/3dmodels/bases/BF3_mod_f4_foot_L_v0100.stl");
-    cargarSTL(1, loader, materialSTL, new THREE.Vector3(30, 0, 0), "/static/3dmodels/bases/BF3_mod_f4_foot_R_v0100.stl");
-    cargarSTL(2, loader, materialSTL, new THREE.Vector3(22, 0, 0), "/static/3dmodels/bases/BF3_module_c3_camp_bot_v0100.stl");
-    cargarSTL(3, loader, materialSTL, new THREE.Vector3(15, 0, 0), "/static/3dmodels/bases/BF3_module_c4_camp_top_v0100.stl");
-    cargarSTL(4, loader, materialSTL, new THREE.Vector3(5, 0, 0), "/static/3dmodels/bases/BF3_module_c5_camp_el_top_v0100.stl");
-    cargarSTL(5, loader, materialSTL, new THREE.Vector3(-10, 0, 0), "/static/3dmodels/bases/BF3_module_c5_c_camp_el_top_v0100.stl");
-    cargarSTL(6, loader, materialSTL, new THREE.Vector3(-25, 0, 0), "/static/3dmodels/bases/BF3_module_f1_foot_v0100.stl");
-    cargarSTL(7, loader, materialSTL, new THREE.Vector3(-35, 0, 0), "/static/3dmodels/bases/BF3_module_f2_foot_v0100.stl");
-    cargarSTL(8, loader, materialSTL, new THREE.Vector3(-45, 0, 0), "/static/3dmodels/bases/BF3_module_f3_foot_v0100.stl");
+    cargarSTL(0, loader, materialSTL, new THREE.Vector3(50, 0, 0), "/static/3dmodels/bases/BF3_mod_f4_foot_L_v0100.stl");
+    cargarSTL(1, loader, materialSTL, new THREE.Vector3(40, 0, 0), "/static/3dmodels/bases/BF3_mod_f4_foot_R_v0100.stl");
+    cargarSTL(2, loader, materialSTL, new THREE.Vector3(32, 0, 0), "/static/3dmodels/bases/BF3_module_c3_camp_bot_v0100.stl");
+    cargarSTL(3, loader, materialSTL, new THREE.Vector3(25, 0, 0), "/static/3dmodels/bases/BF3_module_c4_camp_top_v0100.stl");
+    cargarSTL(4, loader, materialSTL, new THREE.Vector3(15, 0, 0), "/static/3dmodels/bases/BF3_module_c5_camp_el_top_v0100.stl");
+    cargarSTL(5, loader, materialSTL, new THREE.Vector3(0, 0, 0), "/static/3dmodels/bases/BF3_module_c5_c_camp_el_top_v0100.stl");
+    cargarSTL(6, loader, materialSTL, new THREE.Vector3(-15, 0, 0), "/static/3dmodels/bases/BF3_module_f1_foot_v0100.stl");
+    cargarSTL(7, loader, materialSTL, new THREE.Vector3(-25, 0, 0), "/static/3dmodels/bases/BF3_module_f2_foot_v0100.stl");
+    cargarSTL(8, loader, materialSTL, new THREE.Vector3(-35, 0, 0), "/static/3dmodels/bases/BF3_module_f3_foot_v0100.stl");
 
     //conexiones
-    cargarSTL(9, loader, materialSTL, new THREE.Vector3(40, 10, 0), "/static/3dmodels/conexiones/BF3_module_e2_elev_v0100.stl");
-    cargarSTL(10, loader, materialSTL, new THREE.Vector3(30, 13, 0), "/static/3dmodels/conexiones/BF3_module_e1_elev_v0100.stl");
-    cargarSTL(11, loader, materialSTL, new THREE.Vector3(10, 20, 0), "/static/3dmodels/conexiones/BF3_module_e1_c_elev_v0100.stl");
-    cargarSTL(12, loader, materialSTL, new THREE.Vector3(0, 20, 0), "/static/3dmodels/conexiones/BF3_module_c2_camp_out_R_v0100.stl");
-    cargarSTL(13, loader, materialSTL, new THREE.Vector3(-10, 20, 0), "/static/3dmodels/conexiones/BF3_module_c2_camp_out_L_v0100.stl");
-    cargarSTL(14, loader, materialSTL, new THREE.Vector3(-20, 20, 0), "/static/3dmodels/conexiones/BF3_module_a1_angle_20_v0100.stl");
-    cargarSTL(15, loader, materialSTL, new THREE.Vector3(-30, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2f_camp_out_front_R_v0100.stl");
-    cargarSTL(16, loader, materialSTL, new THREE.Vector3(-40, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2_camp_out_08mm_R_v0100.stl");
-    cargarSTL(17, loader, materialSTL, new THREE.Vector3(-50, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2_camp_out_08mm_L_v0100.stl");
-    cargarSTL(18, loader, materialSTL, new THREE.Vector3(-60, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2bd_camp_out_back_doub_R_v0100.stl");
-    cargarSTL(19, loader, materialSTL, new THREE.Vector3(-70, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2bd_camp_out_back_doub_L_v0100.stl");
-    cargarSTL(20, loader, materialSTL, new THREE.Vector3(-80, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2b_camp_out_back_R_v0100.stl");
-    cargarSTL(21, loader, materialSTL, new THREE.Vector3(-90, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2b_camp_out_back_L_v0100.stl");
+    cargarSTL(9, loader, materialSTL, new THREE.Vector3(50, 10, 0), "/static/3dmodels/conexiones/BF3_module_e2_elev_v0100.stl");
+    cargarSTL(10, loader, materialSTL, new THREE.Vector3(40, 13, 0), "/static/3dmodels/conexiones/BF3_module_e1_elev_v0100.stl");
+    cargarSTL(11, loader, materialSTL, new THREE.Vector3(20, 20, 0), "/static/3dmodels/conexiones/BF3_module_e1_c_elev_v0100.stl");
+    cargarSTL(12, loader, materialSTL, new THREE.Vector3(10, 20, 0), "/static/3dmodels/conexiones/BF3_module_c2_camp_out_R_v0100.stl");
+    cargarSTL(13, loader, materialSTL, new THREE.Vector3(-0, 20, 0), "/static/3dmodels/conexiones/BF3_module_c2_camp_out_L_v0100.stl");
+    cargarSTL(14, loader, materialSTL, new THREE.Vector3(-10, 20, 0), "/static/3dmodels/conexiones/BF3_module_a1_angle_20_v0100.stl");
+    cargarSTL(15, loader, materialSTL, new THREE.Vector3(-20, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2f_camp_out_front_R_v0100.stl");
+    cargarSTL(16, loader, materialSTL, new THREE.Vector3(-30, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2_camp_out_08mm_R_v0100.stl");
+    cargarSTL(17, loader, materialSTL, new THREE.Vector3(-40, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2_camp_out_08mm_L_v0100.stl");
+    cargarSTL(18, loader, materialSTL, new THREE.Vector3(-50, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2bd_camp_out_back_doub_R_v0100.stl");
+    cargarSTL(19, loader, materialSTL, new THREE.Vector3(-60, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2bd_camp_out_back_doub_L_v0100.stl");
+    cargarSTL(20, loader, materialSTL, new THREE.Vector3(-70, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2b_camp_out_back_R_v0100.stl");
+    cargarSTL(21, loader, materialSTL, new THREE.Vector3(-80, 20, 0), "/static/3dmodels/conexiones/BF3_mod_c2b_camp_out_back_L_v0100.stl");
 
     //modulos humedad  
-    cargarSTL(22, loader, materialSTL, new THREE.Vector3(-15, 50, 0), "/static/3dmodels/modulosHumedad/BF3_module_h1_hum_L_v0110.stl");
-    cargarSTL(23, loader, materialSTL, new THREE.Vector3(5, 50, 0), "/static/3dmodels/modulosHumedad/BF3_module_h2_c_hum_L_v0110.stl");
+    cargarSTL(22, loader, materialSTL, new THREE.Vector3(-65, 0, 0), "/static/3dmodels/modulosHumedad/BF3_module_h1_hum_L_v0110.stl");
+    cargarSTL(23, loader, materialSTL, new THREE.Vector3(-50, 0, 0), "/static/3dmodels/modulosHumedad/BF3_module_h2_c_hum_L_v0110.stl");
 
     //salasYAccesorios
-    cargarSTL(24, loader, materialSTL, new THREE.Vector3(40, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b1_c_room_v0100.stl");
-    cargarSTL(25, loader, materialSTL, new THREE.Vector3(5, -25, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b1_room_v0100.stl");
-    cargarSTL(26, loader, materialSTL, new THREE.Vector3(-5, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_C_v0100.stl");
+    cargarSTL(24, loader, materialSTL, new THREE.Vector3(50, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b1_c_room_v0100.stl");
+    cargarSTL(25, loader, materialSTL, new THREE.Vector3(15, -25, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b1_room_v0100.stl");
+    cargarSTL(26, loader, materialSTL, new THREE.Vector3(5, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_C_v0100.stl");
 
-    //setTimeout(function(){ ; }, 3000);
-    /*var pos = modelos.length - 1;
-    console.log(pos + "Hola");
-    modelos[0].rotation.x = Math.PI / 2;*/
-    //modelos[0].rotateOnAxis( new THREE.Vector3(1,0,0), 0.1 );
-    cargarSTL(27, loader, materialSTL, new THREE.Vector3(-25, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_L_v0100.stl");
-    cargarSTL(28, loader, materialSTL, new THREE.Vector3(-45, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_R_v0100.stl");
-    cargarSTL(29, loader, materialSTL, new THREE.Vector3(-65, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_C_v0100.stl");
-    cargarSTL(30, loader, materialSTL, new THREE.Vector3(-85, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_L_v0110.stl");
-    cargarSTL(31, loader, materialSTL, new THREE.Vector3(-105, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_R_v0110.stl");
-    cargarSTL(31, loader, materialSTL, new THREE.Vector3(65, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_v0100.stl");
+
+    cargarSTL(27, loader, materialSTL, new THREE.Vector3(-15, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_L_v0100.stl");
+    cargarSTL(28, loader, materialSTL, new THREE.Vector3(-35, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_15_R_v0100.stl");
+    cargarSTL(29, loader, materialSTL, new THREE.Vector3(-55, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_C_v0100.stl");
+    cargarSTL(30, loader, materialSTL, new THREE.Vector3(-75, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_L_v0110.stl");
+    cargarSTL(31, loader, materialSTL, new THREE.Vector3(-95, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_pal_R_v0110.stl");
+    cargarSTL(31, loader, materialSTL, new THREE.Vector3(75, -20, 0), "/static/3dmodels/salasYAccesorios/BF3_module_b2_room_h_v0100.stl");
     //tapasCierre
-    cargarSTL(32, loader, materialSTL, new THREE.Vector3(-97, 20, 0), "/static/3dmodels/tapasCierre/BF3_module_c1_camp_L_v0100.stl");
-    cargarSTL(33, loader, materialSTL, new THREE.Vector3(-105, 20, 0), "/static/3dmodels/tapasCierre/BF3_module_c1_camp_R_v0100.stl");
+    cargarSTL(32, loader, materialSTL, new THREE.Vector3(-87, 20, 0), "/static/3dmodels/tapasCierre/BF3_module_c1_camp_L_v0100.stl");
+    cargarSTL(33, loader, materialSTL, new THREE.Vector3(-95, 20, 0), "/static/3dmodels/tapasCierre/BF3_module_c1_camp_R_v0100.stl");
 
     //tuercas
-    cargarSTL(34, loader, materialSTL, new THREE.Vector3(0, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n1a_nut_A_type1_v0100.stl");
-    cargarSTL(35, loader, materialSTL, new THREE.Vector3(10, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n1b_nut_B_type1_v0100.stl");
-    cargarSTL(36, loader, materialSTL, new THREE.Vector3(-10, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n2a_nut_A_type2_v0100.stl");
-    cargarSTL(37, loader, materialSTL, new THREE.Vector3(-20, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n2b_nut_B_type2_v0100.stl");
-    cargarSTL(38, loader, materialSTL, new THREE.Vector3(20, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n3a_nut_A_type3_v0100.stl");
-    cargarSTL(39, loader, materialSTL, new THREE.Vector3(30, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n3b_nut_B_type3_v0100.stl");
+    cargarSTL(34, loader, materialSTL, new THREE.Vector3(10, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n1a_nut_A_type1_v0100.stl");
+    cargarSTL(35, loader, materialSTL, new THREE.Vector3(20, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n1b_nut_B_type1_v0100.stl");
+    cargarSTL(36, loader, materialSTL, new THREE.Vector3(0, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n2a_nut_A_type2_v0100.stl");
+    cargarSTL(37, loader, materialSTL, new THREE.Vector3(-10, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n2b_nut_B_type2_v0100.stl");
+    cargarSTL(38, loader, materialSTL, new THREE.Vector3(30, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n3a_nut_A_type3_v0100.stl");
+    cargarSTL(39, loader, materialSTL, new THREE.Vector3(40, -30, 0), "/static/3dmodels/tuercas/BF3_mod_n3b_nut_B_type3_v0100.stl");
 
     function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement;
@@ -273,6 +317,7 @@ function main() {
 
     function render(time) {
         //time *= 0.01;  // convert to seconds;
+
 
         if (resizeRendererToDisplaySize(renderer)) {
             const canvas = renderer.domElement;
